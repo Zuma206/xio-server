a51e2e8cd0ee18854741e2de9284e9939af05da7
Zuma206 <Zuma#2517>
2022-11-22 19:11:37 +0000

    Added image check endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-
 import users from "./routes/users";
 import channels from "./routes/channels";
+import { authorize } from "./firebase";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
@@ -10,9 +11,17 @@ app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/channels", channels);
 
-app.get("*", async (req, res) => {
+app.get("*", async (_req, res) => {
     res.send("XIO API");
 });
+
+app.post(
+    "/api/image",
+    authorize(async (req, _userData, result) => {
+        const res = await axios.get(req.body.url);
+        return result(res.status == 200);
+    })
+);
 
 if (!process.env.DETA_RUNTIME) {
     app.listen(PORT, () => console.log(`Started on http://localhost:${PORT}`));
