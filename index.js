dfac46ac306826ae96aad57a4a7e729d46b573fd
Zuma206 <Zuma#2517>
2023-03-04 15:36:45 +0000

    updated to deta space

--- index.js
+++
@@ -1,2 +0,0 @@
-const app = require("./dist/index.js");
-module.exports = app;
--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,9 @@ import { authorize, firebase } from "./firebase";
 import axios from "axios";
 import { userInChannel } from "./database/channels";
 import { pusher } from "./pusher";
-import process from "process";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,45 +15,39 @@ app.use("/api/users", users);
 app.use("/api/channels", channels);
 
 app.get("*", async (req, res) => {
-    res.send(req.url);
+  res.send(req.url);
 });
 
 app.post(
-    "/api/image",
-    authorize(async (req, _userData, result) => {
-        const res = await axios.get(
-            `https://external-content.duckduckgo.com/iu/?u=${encodeURIComponent(
-                req.body.url
-            )}`
-        );
-        return result(res.status == 200);
-    })
+  "/api/image",
+  authorize(async (req, _userData, result) => {
+    const res = await axios.get(
+      `https://external-content.duckduckgo.com/iu/?u=${encodeURIComponent(
+        req.body.url
+      )}`
+    );
+    return result(res.status == 200);
+  })
 );
 
 app.post("/api/auth", async (req, res) => {
-    try {
-        const socketId = req.body.socket_id;
-        const pusherChannel = req.body.channel_name;
-        const userData = await firebase.verifyIdToken(
-            req.headers.authorization
-        );
-        const authorized = await userInChannel(
-            pusherChannel.split("-")[1],
-            userData.uid
-        );
-        if (authorized) {
-            const response = pusher.authorizeChannel(socketId, pusherChannel);
-            res.send(response).end();
-        } else {
-            res.status(403).end();
-        }
-    } catch {
-        res.status(403).end();
+  try {
+    const socketId = req.body.socket_id;
+    const pusherChannel = req.body.channel_name;
+    const userData = await firebase.verifyIdToken(req.headers.authorization);
+    const authorized = await userInChannel(
+      pusherChannel.split("-")[1],
+      userData.uid
+    );
+    if (authorized) {
+      const response = pusher.authorizeChannel(socketId, pusherChannel);
+      res.send(response).end();
+    } else {
+      res.status(403).end();
     }
+  } catch {
+    res.status(403).end();
+  }
 });
 
-if (process.argv.length > 2 && process.argv[2] == "boot") {
-    app.listen(PORT, () => console.log(`Started on http://localhost:${PORT}`));
-}
-
-module.exports = app;
+app.listen(PORT, () => console.log(`Started on http://localhost:${PORT}`));
