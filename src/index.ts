c0f88db357356389a7d81668881b7fd584f53167
Zuma206 <Zuma#2517>
2023-03-05 12:50:02 +0000

    fixed dev server not working due to port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { userInChannel } from "./database/channels";
 import { pusher } from "./pusher";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
